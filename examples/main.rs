c42842368a66b4fa943825db7db9f7aff7fafe59
Aaron Trent <novacrazy@gmail.com>
2017-01-21 16:00:54 -0600

    Update example

--- examples/main.rs
+++ examples/main.rs
@@ -1,51 +1,30 @@
 extern crate tinyfiledialogs;
 
-use tinyfiledialogs::{YesNo, MessageBoxIcon, DefaultColorValue};
+use tinyfiledialogs::{DefaultColorValue, MessageBox, Icon, BoxButton};
 
 fn main() {
+    let choice = tinyfiledialogs::message_box(MessageBox::YesNo, "hello", "yes or no?",
+                                              // Icon
+                                              Some(Icon::Question),
+                                              // Default button
+                                              Some(BoxButton::CancelNo));
 
-    let choice = tinyfiledialogs::message_box_yes_no("hello", "yes or no?",
-                                                     MessageBoxIcon::Question, YesNo::No);
+    let user_input = tinyfiledialogs::input_box("Enter user name", "Username:", "");
 
-    let user_input: String;
-    match tinyfiledialogs::input_box("Enter user name", "Username:", "") {
-        Some(input) => user_input = input,
-        None => user_input = "null".to_string(),
-    }
+    let save_file = tinyfiledialogs::save_file_dialog("Save", "password.txt");
 
-    let save_file: String;
-    match tinyfiledialogs::save_file_dialog("Save", "password.txt") {
-        Some(file) => save_file = file,
-        None => save_file = "null".to_string(),
-    }
+    let open_file = tinyfiledialogs::open_file_dialog("Open", "password.txt", None);
 
-    let open_file: String;
-    match tinyfiledialogs::open_file_dialog("Open", "password.txt", None) {
-        Some(file) => open_file = file,
-        None => open_file = "null".to_string(),
-    }
+    let folder = tinyfiledialogs::select_folder_dialog("Select folder", "");
 
-    let folder: String;
-    match tinyfiledialogs::select_folder_dialog("Select folder", "") {
-        Some(result) => folder = result,
-        None => folder = "null".to_string(),
-    }
+    let color = tinyfiledialogs::color_chooser_dialog("Choose a Color", DefaultColorValue::Hex("#FF0000"));
 
-    let color: String;
-    match tinyfiledialogs::color_chooser_dialog("Choose a Color", DefaultColorValue::Hex("#FF0000")) {
-        Some((hex_result, _rgb)) => color = hex_result,
-        None => color = "null".to_string(),
-    }
-
-    let list: String;
-    match tinyfiledialogs::list_dialog("Test Dialog",
-                                       &["Id", "Name"],
-                                       Some(&["471", "Donald Duck",
-                                              "1143", "Chris P. Bacon",
-                                              "6509", "Moon Doge"])) {
-        Some(result) => list = result,
-        None => list = "null".to_string(),
-    }
+    #[cfg(not(windows))]
+    let list = tinyfiledialogs::list_dialog("Test Dialog",
+                                            &["Id", "Name"],
+                                            Some(&["471", "Donald Duck",
+                                                "1143", "Chris P. Bacon",
+                                                "6509", "Moon Doge"]));
 
     println!("Choice {:?}", choice);
     println!("User input {:?}", user_input);
@@ -53,5 +32,7 @@ fn main() {
     println!("Open file {:?}", open_file);
     println!("folder {:?}", folder);
     println!("color {:?}", color);
+
+    #[cfg(not(windows))]
     println!("List {:?}", list);
 }
