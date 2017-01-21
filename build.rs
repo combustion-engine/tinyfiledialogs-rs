cf329e3cd3a84114ab7ba0c841c668068078cbf8
Aaron Trent <novacrazy@gmail.com>
2017-01-21 15:52:00 -0600

    Cleanup build script

--- build.rs
+++ build.rs
@@ -1,20 +1,11 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 extern crate gcc;
 
 use std::env;
 
 fn main() {
-    let target = env::var("TARGET").unwrap();
+    gcc::compile_library("libtinyfiledialogs.a", &["libtinyfiledialogs/tinyfiledialogs.c"]);
 
-    let mut cfg = gcc::Config::new();
-    cfg.file("libtinyfiledialogs/tinyfiledialogs.c");
-    cfg.flag("-v");
-    cfg.compile("libtinyfiledialogs.a");
-
-    if target.contains("windows") {
+    if env::var("TARGET").unwrap().contains("windows") {
         println!("cargo:rustc-link-lib=user32");
         println!("cargo:rustc-link-lib=comdlg32");
         println!("cargo:rustc-link-lib=ole32");
